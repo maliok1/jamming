90bbe33d4b62182558ef617aa8bef5ffc484e5ee
Anastasia Mal <lisanastya15@gmail.com>
2020-06-19 14:38:50 +0200

    structured components, added some communication

---
+++ src/Components/TrackList/TrackList.js
@@ -0,0 +1,22 @@
+import React from "react";
+import "./TrackList.css";
+import Track from "../Track/Track";
+
+class TrackList extends React.Component {
+  render() {
+    const { tracks } = this.props;
+       
+    return (
+      <div className="TrackList">
+        {
+          tracks.map((track) => {
+            return <Track track = {track}
+                          key={track.id} />
+          })
+        }
+      </div>
+    );
+  }
+}
+
+export default TrackList;
